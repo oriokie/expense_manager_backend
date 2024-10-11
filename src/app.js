10482381d9627fdd697fa11c4c32dbfe14f66f15
Edwin <oriokie@icloud.com>
2024-10-11 13:09:00 +0300

    edited allowed origins

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(limiter);
 app.options('*', cors()); // handle preflight requests
 
 // CORS configuration
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+const allowedOrigins = [process.env.FRONTEND_URL, 'https://oriokie.tech', 'http://localhost:3000'];
 
 app.use(
   cors({
